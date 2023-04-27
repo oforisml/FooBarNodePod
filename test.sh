4cf5e3553cd87eb636c99596517a040291377c7b
Samuel Ofori <oforisml@gmail.com>
2023-04-27 23:16:12 +0000

    Randomly sending traffic

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
 
+for i in {1..1000}
+do
+  # Randomly Generating a random number between 1 and 2
+  rand=$((RANDOM % 2 + 1))
 
-for _ in {1..1000}
-    do
-        curl -s http://foo.localhost
-        curl -s http://bar.localhost
-    done
+  # The random number is used to determine which URL to request
+  if [ $rand -eq 1 ]
+  then
+    curl foo.localhost
+  else
+    curl bar.localhost
+  fi
+done
